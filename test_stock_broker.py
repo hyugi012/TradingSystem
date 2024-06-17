72cfad6122d359cb1570b6930a7a3765bc7ec3cc
sanghyuk.lee <sogood.lee@samsung.com>
2024-06-17 11:59:22 +0900

    [unittest] unittest code for get_price

--- test_stock_broker.py
+++ test_stock_broker.py
@@ -1,6 +1,16 @@
 from unittest import TestCase
+from unittest.mock import Mock, patch
+
+from stock_broker_application import StockBrokerApplication
 
 
 class TestStockBroker(TestCase):
     def test_login(self):
         pass
+
+    def test_get_price(self):
+        mk = Mock()
+        ts = StockBrokerApplication(mk)
+        mk.current_stock_price.return_value = 53000
+
+        self.assertEqual(ts.get_price(123), 53000)
