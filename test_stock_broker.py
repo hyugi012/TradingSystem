cea5e6db9b6a7c79a14a830baf33c1796459f1bc
heehoha <dolgyuk@gmail.com>
2024-06-17 13:15:40 +0900

    [feature] test_login 작성

--- test_stock_broker.py
+++ test_stock_broker.py
@@ -1,6 +1,25 @@
+import io
+
+from contextlib import redirect_stdout
 from unittest import TestCase
+
+from stock_broker import KiwerStockBroker, NemoStockBroker
+from stock_broker_application import StockBrokerApplication
 
 
 class TestStockBroker(TestCase):
     def test_login(self):
-        pass
+        ID = "D_Team"
+        PSWD = "changeme"
+
+        test_cases = [(KiwerStockBroker(), f"{ID} login success"),
+                      (NemoStockBroker(), f"[NEMO]{ID} login GOOD"),
+                      ]
+
+        for stock_broker, login_message in test_cases:
+            with self.subTest(f"{stock_broker.__class__.__name__} login Test!"):
+                app = StockBrokerApplication(stock_broker)
+                with io.StringIO() as buf, redirect_stdout(buf):
+                    app.login(ID, PSWD)
+                    captured_stdout = buf.getvalue().replace("\n", "")
+                    self.assertEqual(captured_stdout, login_message)
