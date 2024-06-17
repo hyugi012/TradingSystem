dafd74ab65ba67794cff1c186c95f3f9e9013422
Sunghwaa <sunghwaa.lee@gmail.com>
2024-06-17 14:02:11 +0900

    [refactoring] 중복 코드 개선

--- test_stock_broker.py
+++ test_stock_broker.py
@@ -6,9 +6,13 @@ from stock_broker_application import StockBrokerApplication
 
 
 class TestStockBroker(TestCase):
-
     def sell_mock_function(self, stock_code, price, amount):
         print('[Mock]' + stock_code + ' : Sell stock ( ' + str(price) + ' * ' + str(amount) + ' )')
+
+    def sell_test_function(self, broker):
+        sut = StockBrokerApplication(broker)
+
+        sut.sell(123, 10000, 456)
 
     def test_login(self):
         pass
@@ -16,15 +20,12 @@ class TestStockBroker(TestCase):
     def test_sell_with_mock_broker(self):
         broker_mock = Mock()
         broker_mock.sell.side_effect = self.sell_mock_function
-        sut = StockBrokerApplication(broker_mock)
 
-        sut.sell(123, 10000, 456)
+        self.sell_test_function(broker_mock)
 
         broker_mock.sell.assert_called_once()
 
     def test_sell_with_specific_broker(self):
         for broker in [NemoStockBroker(), KiwerStockBroker()]:
             with self.subTest(f'Broker: {broker.__class__.__name__}'):
-                sut = StockBrokerApplication(broker)
-
-                sut.sell(123, 10000, 456)
+                self.sell_test_function(broker)
