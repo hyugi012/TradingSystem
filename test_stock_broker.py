954604bdd153cd35ff3ca3e3fc40148f5552c19c
sanghyuk.lee <sogood.lee@samsung.com>
2024-06-17 14:07:47 +0900

    [unittest] unittest code for get_price

--- test_stock_broker.py
+++ test_stock_broker.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 from unittest.mock import Mock, patch
 
+from kiwer_api import KiwerAPI
+from stock_broker import KiwerStockBroker
 from stock_broker_application import StockBrokerApplication
 
 
@@ -8,9 +10,8 @@ class TestStockBroker(TestCase):
     def test_login(self):
         pass
 
-    def test_get_price(self):
-        mk = Mock()
-        ts = StockBrokerApplication(mk)
-        mk.current_stock_price.return_value = 53000
+    @patch.object(KiwerAPI, 'current_price', return_value=53000)
+    def test_get_price(self, mock):
+        ts = StockBrokerApplication(KiwerStockBroker())
 
         self.assertEqual(ts.get_price(123), 53000)
