933b45add9aaed197b2f43392dd71c5166c8ae40
Sunghwaa <sunghwaa.lee@gmail.com>
2024-06-17 13:58:58 +0900

    [feature] 개별 broker 테스트 추가

--- test_stock_broker.py
+++ test_stock_broker.py
@@ -8,12 +8,12 @@ from stock_broker_application import StockBrokerApplication
 class TestStockBroker(TestCase):
 
     def sell_mock_function(self, stock_code, price, amount):
-        print(stock_code + ' : Sell stock ( ' + str(price) + ' * ' + str(amount))
+        print('[Mock]' + stock_code + ' : Sell stock ( ' + str(price) + ' * ' + str(amount) + ' )')
 
     def test_login(self):
         pass
 
-    def test_sell_broker(self):
+    def test_sell_with_mock_broker(self):
         broker_mock = Mock()
         broker_mock.sell.side_effect = self.sell_mock_function
         sut = StockBrokerApplication(broker_mock)
@@ -21,3 +21,10 @@ class TestStockBroker(TestCase):
         sut.sell(123, 10000, 456)
 
         broker_mock.sell.assert_called_once()
+
+    def test_sell_with_specific_broker(self):
+        for broker in [NemoStockBroker(), KiwerStockBroker()]:
+            with self.subTest(f'Broker: {broker.__class__.__name__}'):
+                sut = StockBrokerApplication(broker)
+
+                sut.sell(123, 10000, 456)
